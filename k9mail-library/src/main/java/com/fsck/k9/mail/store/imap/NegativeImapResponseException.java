ed79a3af2a7f3587858259659ec02d17ad8305a9
Philip Whitehouse <philip@whiuk.com>
2016-09-19 22:13:36 +0100

    Fix ImapResponse

--- k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/NegativeImapResponseException.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/NegativeImapResponseException.java
@@ -5,9 +5,11 @@ import com.fsck.k9.mail.MessagingException;
 class NegativeImapResponseException extends MessagingException {
     private static final long serialVersionUID = 3725007182205882394L;
 
+    private final String alertText;
 
-    public NegativeImapResponseException(String message) {
+    public NegativeImapResponseException(String message, String alertText) {
         super(message, true);
+        this.alertText = alertText;
     }
 
     public String getAlertText() {
